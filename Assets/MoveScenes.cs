00dafe2b7e5aecfe0bd8b8615e46e91d31fb02d9
Tyler87-sudo <tj.vandijk@student.alfa-college.nl>
2024-03-12 11:35:59 +0100

    First Commit

---
+++ Assets/MoveScenes.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MoveScenes : MonoBehaviour
+{
+    [SerializeField] private string newLevel;
+    // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("Scenes/Scene2");
+            Console.WriteLine("HOIIII");
+        }
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
